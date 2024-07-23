59d142273fb3a403f786e004eba3e585df1ff347
Channa Maduranga <83961725+ChannaMaduranga@users.noreply.github.com>
2024-07-24 02:40:09 +0530

    remove repo name from index.html

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
-  resolve: {
-    alias: {
-      '@fortawesome/fontawesome-free': 'node_modules/@fortawesome/fontawesome-free'
-    }
-  }
+  // base: '/',
+  // resolve: {
+  //   alias: {
+  //     '@fortawesome/fontawesome-free': 'node_modules/@fortawesome/fontawesome-free'
+  //   }
+  // }
   
 })
