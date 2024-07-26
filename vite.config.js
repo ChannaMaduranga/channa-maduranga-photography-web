e480f13c8f642f48ab77cad3b923e0e7fec0a21b
Channa Maduranga <83961725+ChannaMaduranga@users.noreply.github.com>
2024-07-27 02:41:54 +0530

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/",
+  base:"/channa-maduranga-photography-web/",
   resolve: {
     alias: {
       '@fortawesome/fontawesome-free': 'node_modules/@fortawesome/fontawesome-free'
