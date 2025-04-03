4d73729edae9ca027847b1ff08466b4aebb77635
Channa Maduranga <83961725+ChannaMaduranga@users.noreply.github.com>
2025-04-03 10:52:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,23 @@ export default defineConfig({
   plugins: [react()],
   base:"",
   // for use localhost
-  // base:"/channa-maduranga-photography-web/",
+  //base:"/channa-maduranga-photography-web/",
   resolve: {
     alias: {
       '@fortawesome/fontawesome-free': 'node_modules/@fortawesome/fontawesome-free'
     }
-  }
+  },
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true, // Ensure fallback to index.html
+  },
+  esbuild: {
+    minify: true,
+  },
+  buildEnd: () => {
+    fs.copyFileSync('dist/index.html', 'dist/404.html');
+  },
   
 })
